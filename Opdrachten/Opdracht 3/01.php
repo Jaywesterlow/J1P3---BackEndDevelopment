d61956ce639da1b1d31cbdfa135ec1f1820d4307
Jaymar Westerlow <jaywesterlow71@gmail.com>
2021-03-05 14:25:06 +0100

    If statements

--- Opdrachten/Opdracht 3/01.php
+++ Opdrachten/Opdracht 3/01.php
@@ -3,14 +3,19 @@
 <style>
 body{
     font-size: 30px;
-    margin-left:20%;
+    margin-left:1%;
+    background-color: #2C3E50;
+    color: #48C9B0;
 }
+
+
+
 </style>
 
 <?php
 
-$getal1 = 2;
-$getal2 = 6;
+$getal1 = 3;
+$getal2 = 3;
 $getal3 = $getal1 + $getal2;
 $getal4 = $getal1 - $getal2;
 $getal5 = $getal1 / $getal2;
@@ -27,4 +32,22 @@ echo "$getal6 <br>";
 echo "$getal7 <br>";
 echo "$getal8 <br>";
 echo "$getal9 <br>";
+
+
+if ($getal1 > $getal2){
+    echo "getal 1 is groter dan getal 2";
+}
+elseif($getal1 < $getal2){
+    echo "getal 1 is kleiner dan getal 2";
+}
+elseif($getal1 === $getal2){
+    echo "getal 1 is gelijk aan getal 2";
+}
+else{
+    echo "alle vergelijkingen zijn onjuist";
+}
+
 ?>
+
+</body>
+</html>
